a8a3075ec8a18f94b09a4b2497cdf4b9d3bd5af8
zhakhalov <zhakhalov@gmail.com>
2015-02-09 22:45:29 +0200

    Initial commit

---
+++ TaskBoard.WebUI/Bootstrapper.cs
@@ -0,0 +1,51 @@
+using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using Unity.Mvc4;
+using Unity.WebApi;
+using System.Web.Http;
+using System.Configuration;
+
+namespace TaskBoard.WebUI
+{
+    public static class Bootstrapper
+    {
+        public static IUnityContainer Initialise()
+        {
+            var mvcContainer = BuildUnityMvcContainer();
+            var webApiContainer = BuildUnityMvcContainer();
+
+            DependencyResolver.SetResolver(new Unity.Mvc4.UnityDependencyResolver(mvcContainer));
+            GlobalConfiguration.Configuration.DependencyResolver = new Unity.WebApi.UnityDependencyResolver(mvcContainer);
+
+            return mvcContainer;
+        }
+
+        private static IUnityContainer BuildUnityMvcContainer()
+        {
+            var container = new UnityContainer();
+
+            RegisterMvcTypes(container);
+
+            return container;
+        }
+
+        public static void RegisterMvcTypes(IUnityContainer container)
+        {
+
+        }
+
+        private static IUnityContainer BuildUnityWebApiContainer()
+        {
+            var container = new UnityContainer();
+            RegisterWebApiTypes(container);
+
+            return container;
+        }
+
+        public static void RegisterWebApiTypes(IUnityContainer container)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["TaskBoard"].ConnectionString;
+
+        }
+    }
+}
